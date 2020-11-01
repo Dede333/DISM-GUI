46ff0f06d83516d637a9950e2e08369cb72151f5
Dede333 <61848651+Dede333@users.noreply.github.com>
2020-11-01 22:50:52 +0100

    Add files via upload

---
+++ WindowsPrincipal.cs
@@ -0,0 +1,12 @@
+﻿namespace DISM_GUI
+{
+    internal class WindowsPrincipal
+    {
+        private object p;
+
+        public WindowsPrincipal(object p)
+        {
+            this.p = p;
+        }
+    }
+}
